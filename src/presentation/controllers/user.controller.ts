9eb8c5dbd5c9d7903c011e7a41107268a48becf9
Julio Brazao <julio@brazao.dev>
2025-07-02 20:31:13 -0400

    Import adjustments

--- src/presentation/controllers/user.controller.ts
+++ src/presentation/controllers/user.controller.ts
@@ -1,12 +1,14 @@
 import { CreateUserUseCase } from '@/domain/usecases/create-user.usecase';
 import { Controller, Post } from '@nestjs/common';
+import { CreateUserRequestDTO } from '@/presentation/dtos/create-user.request.dto';
+import { CreateUserResponseDTO } from '@/presentation/dtos/create-user.response.dto';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly createUserUseCase: CreateUserUseCase) {}
 
   @Post()
-  async create(params: any): Promise<any> {
+  async create(params: CreateUserRequestDTO): Promise<CreateUserResponseDTO> {
     try {
       return await this.createUserUseCase.execute(params);
     } catch (err) {
